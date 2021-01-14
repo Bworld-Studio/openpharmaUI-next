e4d894955912455f050d814186a370f31c27e9f4
Gray Fawkes <64316552+GrayFawkes@users.noreply.github.com>
2021-01-14 19:24:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# openpharmaUI-next
+ 
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+	server: {
+		host: 'localhost',
+		port: 8080,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
+}
