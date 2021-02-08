849fa7e39c53f65a0b3e8fc0a32689989e997a68
Shnappy31 <shnapps@gmail.com>
2021-02-08 19:45:39 +0100

    :construction: creation vue product

--- src/components/products/Products.vue
+++ src/components/products/Products.vue
@@ -3,17 +3,14 @@
 		<div>
 			<div class="">
 				<h2 class="title">{{$t('products.title')}}</h2>
-				<!-- <table class="table">
-					<tr v-for="(line) in clients" v-bind:key="line.uuid" v-bind:title="line.numSS">
-						<td class="text-left">{{line.lastName}}</td>
-						<td class="text-left">{{line.firstName}}</td>
-						<td class="text-left">{{ $d(new Date(line.birthDate), "short") }}</td>
-						<td class="text right">
-							<button class="btn btn-info" v-on:click="editClient(line)">{{$t('buttons.edit-button')}}</button>
-							<button class="btn btn-danger" v-on:click="deleteClient(line.uuid)">{{$t('buttons.delete-button')}}</button>
-						</td>
+					<button class="btn btn-info" v-on:click="getProducts()">{{$t('buttons.edit-button')}}</button>
+				 <table class="table">
+					<tr v-for="(line) in products" v-bind:key="line.cis" v-bind:title="line.labelMed">
+						<td class="text-left">{{line.labelMed}}</td>
+						<td class="text-left">{{line.pharmaForm}}</td>
+						
 					</tr>
-				</table> -->
+				</table>
 			</div>
 		</div>
 	</div>
@@ -23,30 +20,21 @@
 </style>
 
 <script>
-// import axios from 'axios'
+ import axios from 'axios'
 
 export default {
-	name: 'Products',
-	data () {
-		return {}
-	},
-	mounted () {
-		this.getProducts()
-	},
-	methods: {
-		getProducts () {
-
-			// Call API
-			// axios.get('/api/clients').then(
-			// 	result => {
-			// 		console.log(result.data)
-			// 		this.clients = result.data
-			// 	},
-			// 	error => {
-			// 		console.error(error)
-			// 	}
-			// )
-		}
+setup() {
+	const products = []
+	const url = '/api/products/'
+	const getProducts = function () {
+		axios.get(url).then(result => {
+			products = result
+		}).catch(error =>{console.log(error)})
 	}
+	return {
+		products,
+		getProducts
+	}
+}
 }
 </script>
